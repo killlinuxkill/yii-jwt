fa63787bd0531305d54cabe9d586c46747123ea8
killwindows <killlinuxkill@gmail.com>
2018-08-23 13:17:26 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # yii-jwt
+    INSTALL
+        composer require killlinuxkill/yii-jwt
+
+    CONFIG
+        'components'=>array(
+            ...
+            'JWT' => array(
+                'class' => 'killlinuxkill\JWT',
+                'key' => 'YOUR-SECRET-KEY',
+                'alg' => 'DEFAULT-ALG'
+            ),
+            ...
+        ),
+
+    USE
---
+++ src/JWT.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace killlinuxkill\JWT;
+
+use Firebase\JWT\JWT as BaseJWT;
+
+class JWT
+{
+    public $key = '';
+    public $alg = 'HS256';
+
+    public function init()
+    {}
+
+    public function decode($jwt, $key = '', array $allowed_algs = array())
+    {
+        if (empty($key) && !empty($this->key)) {
+            $key = $this->key;
+        }
+        return BaseJWT::decode($jwt, $key, $allowed_algs);
+    }
+
+    public function encode($payload, $key = '', $alg = 'HS256', $keyId = null, $head = null)
+    {
+        if (empty($key) && !empty($this->key)) {
+            $key = $this->key;
+        }
+        if (empty($alg) && !empty($this->alg)) {
+            $alg = $this->alg;
+        }
+        return BaseJWT::encode($payload, $key, $alg, $keyId, $head);
+    }
+}
